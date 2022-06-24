cbc70753a6334657fcba08a2e95ddeabe98ee76a
KirillMolotkov <balbes_goda@mail>
2022-06-24 12:54:16 +0300

    feat: complite task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const abs = Math.abs(n);
+    const value = String(abs).split("").reverse().join("");
+    return Number(value);
+};
